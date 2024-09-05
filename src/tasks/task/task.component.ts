2e3f9fabbd69259521d997e7ca1a57120647a92f
Balázs Réka <balazsreka@ktch.hu>
2024-09-05 06:42:29 +0200

    user lists works

--- src/tasks/task/task.component.ts
+++ src/tasks/task/task.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+
+}
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css'],
   standalone: true,
+  
 })
 export class TaskComponent {
-
+    @Input({required: true}) task!: Task;
 }
