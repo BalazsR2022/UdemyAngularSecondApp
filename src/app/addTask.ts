2c363c7515e5694ef835fdc886a7d7d58659b4ee
Balázs Réka <balazsreka@ktch.hu>
2024-09-07 12:05:11 +0200

    my solution-new tasks added

---
+++ src/app/addTask.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { TasksComponent } from 'src/tasks/tasks.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewTaskService {
+
+  constructor() { }
+}
--- src/tasks/tasks.component.ts
+++ src/tasks/tasks.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
   standalone: true,
-  imports:[TaskComponent, NgFor, CommonModule],
+  imports:[TaskComponent, NgFor, CommonModule,FormsModule],
   
 })
 export class TasksComponent {
@@ -36,13 +37,38 @@ export class TasksComponent {
         'Prepare and describe an issue template which will help with project management',
       dueDate: '2024-06-15',
     },
-  ]
+  ];
+
+  newTask = {
+    id: '',
+    title: '',
+    summary: '',
+    dueDate: ''
+  }
+
   get selectedUserTasks(){
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 
   onCompleteTask(id: string){
      this.tasks = this.tasks.filter((task)=>task.id !==id);
+  }
+  addTasks(){
+    if (this.newTask.id && this.newTask.title && this.newTask.summary && this.newTask.dueDate) {
+      const taskToAdd = { ...this.newTask, userId: this.userId };
+      this.tasks.push(taskToAdd);
+
+      // Clear input fields after adding the task
+      this.newTask = {
+        id: '',
+        title: '',
+        summary: '',
+        dueDate: ''
+      };
+    } else {
+      alert('Please fill in all fields!');
+    }
+
   }
 
 }
