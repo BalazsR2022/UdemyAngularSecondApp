119722f98b48a3938ef99c59dbf9594d698140df
Balázs Réka <balazsreka@ktch.hu>
2024-09-09 19:35:21 +0200

    card shared component added

--- src/tasks/task/task.component.ts
+++ src/tasks/task/task.component.ts
@@ -2,12 +2,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgFor } from '@angular/common';
 
 import {type Task} from './task.model'
+import { CardComponent } from "../../app/shared/card/card.component";
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css'],
   standalone: true,
+  imports: [CardComponent],
   
 })
 export class TaskComponent {
